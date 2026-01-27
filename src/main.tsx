6c5641f28ece8fa5799775c3a79cd2b9b9106cbb
nathan-perkins <140360768+nathan-perkins@users.noreply.github.com>
2026-01-26 19:27:58 -0600

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import Router from './routes/Router.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <Router />
     </BrowserRouter>
