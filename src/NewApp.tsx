7dcd06e7007fcdca806314f562864ade55dacee8
nathan-perkins <140360768+nathan-perkins@users.noreply.github.com>
2026-06-01 17:29:35 -0500

    stage: app head component

--- src/NewApp.tsx
+++ src/NewApp.tsx
@@ -1,3 +1,15 @@
+import { useState, useRef } from 'react'
+import AppHead from './components/AppHead'
+import './NewApp.css'
+
 export default function NewApp() {
-  return <div>New App</div>
+  const [headHeight, setHeadHeight] = useState(0)
+  const headRef = useRef<HTMLDivElement>(null)
+
+  return (
+    <div className="new-app-container" style={{ paddingTop: `calc(${headHeight}px + 1.5rem)` }}>
+      <AppHead ref={headRef} setHeadHeight={setHeadHeight} />
+      <div>content</div>
+    </div>
+  )
 }
