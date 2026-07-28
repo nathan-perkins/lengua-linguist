4de9e9b91f6926b634da592b830d95314c7066b4
nathan-perkins <140360768+nathan-perkins@users.noreply.github.com>
2026-07-28 16:59:14 -0500

    revamp: strip down the app to its router

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router'
-import './css/App.css'
 
 function Home() {
   return (
--- testSetup.ts
+++ testSetup.ts
@@ -1,15 +1,15 @@
-import {
-  expect, beforeAll, afterEach, afterAll
-} from 'vitest'
-import { cleanup } from '@testing-library/react'
-import * as matchers from '@testing-library/jest-dom/matchers'
-import { server } from './src/mocks/node'
+// import {
+//   expect, beforeAll, afterEach, afterAll
+// } from 'vitest'
+// import { cleanup } from '@testing-library/react'
+// import * as matchers from '@testing-library/jest-dom/matchers'
+// import { server } from './src/mocks/node'
 
-expect.extend(matchers)
+// expect.extend(matchers)
 
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
-afterEach(() => {
-  server.resetHandlers()
-  cleanup()
-})
-afterAll(() => server.close())
+// beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+// afterEach(() => {
+//   server.resetHandlers()
+//   cleanup()
+// })
+// afterAll(() => server.close())
