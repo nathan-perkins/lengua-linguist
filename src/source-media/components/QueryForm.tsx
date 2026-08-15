d7ca2b2a9cf0d6336502e980c9a8bf3a9c7e43ff
nathan-perkins <140360768+nathan-perkins@users.noreply.github.com>
2026-08-14 22:30:52 -0500

    chore: move hook call to source media page

--- src/source-media/components/QueryForm.tsx
+++ src/source-media/components/QueryForm.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
-import { useVideoQuery } from '../hooks/useVideoQuery'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import '../css/QueryForm.css'
 
-export default function QueryForm() {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [submittedQuery, setSubmittedQuery] = useState('')
+type QueryFormProps = {
+  setSubmittedQuery: React.Dispatch<React.SetStateAction<string>>
+}
 
-  const { videos, isPending, isError, error } = useVideoQuery(submittedQuery)
+export default function QueryForm({ setSubmittedQuery }: QueryFormProps) {
+  const [searchQuery, setSearchQuery] = useState('')
 
   const handleQuery = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -17,34 +17,22 @@ export default function QueryForm() {
   }
 
   return (
-    <>
-      <form onSubmit={(e) => void handleQuery(e)} className="query-form">
-        <label>
-          Explore target language videos
-          <div className="search-wrapper">
-            <input
-              type="text"
-              className="searchbar"
-              value={searchQuery}
-              placeholder="Search videos or paste YouTube URL"
-              onChange={({ target }) => setSearchQuery(target.value)}
-            />
-            <button type="submit" className="search-icon-btn">
-              <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass} />
-            </button>
-          </div>
-        </label>
-      </form>
-
-      {isPending && <p>Loading...</p>}
-      {isError && <p>{String(error)}</p>}
-      {!isPending && !isError && (
-        <ul>
-          {videos.map((video) => (
-            <li key={video.id.videoId}>{video.snippet.title}</li>
-          ))}
-        </ul>
-      )}
-    </>
+    <form onSubmit={(e) => void handleQuery(e)} className="query-form">
+      <label>
+        Explore target language videos
+        <div className="search-wrapper">
+          <input
+            type="text"
+            className="searchbar"
+            value={searchQuery}
+            placeholder="Search videos or paste YouTube URL"
+            onChange={({ target }) => setSearchQuery(target.value)}
+          />
+          <button type="submit" className="search-icon-btn">
+            <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass} />
+          </button>
+        </div>
+      </label>
+    </form>
   )
 }
--- src/source-media/pages/SourceMedia.tsx
+++ src/source-media/pages/SourceMedia.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+import { useVideoQuery } from '../hooks/useVideoQuery'
 import QueryForm from '../components/QueryForm'
 import '../css/SourceMedia.css'
 
 export default function SourceMedia() {
+  const [submittedQuery, setSubmittedQuery] = useState('')
+  const { videos, isError, error } = useVideoQuery(submittedQuery)
+
+  if (isError) throw new Error(String(error))
+
+  console.log(videos)
+
   return (
     <div className="source-media">
-      <QueryForm />
+      <QueryForm setSubmittedQuery={setSubmittedQuery} />
     </div>
   )
 }
